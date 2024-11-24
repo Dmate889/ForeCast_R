e87ca9319b359b5193392c2cc5cbf059370599c3
FireWolfy89 <dmate899@gmail.com>
2024-11-24 20:13:04 +0100

    Login functionality added

--- ForeCast_R_frontend/src/app/login-page/login-page.component.ts
+++ ForeCast_R_frontend/src/app/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LoginServiceService } from '../services/login-service.service';
+
 
 @Component({
   selector: 'app-login-page',
@@ -9,6 +11,7 @@ import { Component } from '@angular/core';
 })
 export class LoginPageComponent {
 
+  constructor(private loginservice: LoginServiceService){}
 
   loginUser(event:any){
     event.preventDefault();
@@ -16,7 +19,18 @@ export class LoginPageComponent {
     const username = event.target.elements.username.value.trim();
     const password = event.target.elements.password.value.trim();
 
-    
+    this.loginservice.loginUser(username,password).subscribe( response => {
+      console.log('User logged in!');
+      
+      const token = response.token;
+      localStorage.setItem('jwtToken', token);
 
-  }
+    },
+
+    errr =>{
+      alert('Invalid username or password');
+    }
+  )};
+
+  
 }
