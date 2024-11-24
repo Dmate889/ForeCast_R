5a5ba56fbabfd600e2580cadc0749ccd5573d598
FireWolfy89 <dmate899@gmail.com>
2024-11-24 20:05:51 +0100

    Login service created

--- ForeCast_R_frontend/src/app/services/login-service.service.ts
+++ ForeCast_R_frontend/src/app/services/login-service.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  LoginApiEndpointUrl = 'http://localhost:3000/login';
+  
+  loginUser(username: string, password: string): Observable<any>{
+    const body = {username, password};
+    return this.http.post(this.LoginApiEndpointUrl, body);
+  }
+
 }
